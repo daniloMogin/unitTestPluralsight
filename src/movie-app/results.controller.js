2f0a3b1e473011fe9fa476dcd389b8eaeb615ca0
Danilo Mogin <danilo.mogin@gmail.com>
2016-05-06 15:45:18 +0200

    inicijalni komit unit test pluralsight

---
+++ src/movie-app/results.controller.js
@@ -0,0 +1,15 @@
+(function() {
+'use strict';
+
+    angular.module('movieApp')
+        .controller('ResultsController', function ($scope, $location, omdbApi) {
+            var query = $location.search().q;
+            omdbApi.search(query)
+            .then(function (data) {
+                $scope.results = data.Search
+            })
+            .catch(function () {
+                $scope.errorMessage = 'Something went wrong!'
+            });
+        });
+})(); 
---
+++ src/movie-app/search.controller.js
@@ -0,0 +1,12 @@
+(function() {
+'use strict';
+
+    angular.module('movieApp')
+        .controller('SearchController', function ($scope, $location) {
+            $scope.search = function () {
+                if($scope.query) {
+                    $location.path('/results').search('q', $scope.query);
+                }
+            }
+        });
+})();
